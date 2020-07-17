b18e2fcd8990c701f4c75de570c44e52f7dc442c
LavenderTheGreat <dach.soa@gmail.com>
2020-07-17 19:21:11 +0100

    e I missed

--- palette.js
+++ palette.js
@@ -19,18 +19,19 @@ function toPNG(bytes, isSonic){
     }
 
     // get two bytes we need
-    let colour = bytes[0] + bytes[1]
+    let colour = bytes[1] + bytes[0]
     // make binary by converting to a hex number, then converting to binary as a string, then pad it
     colour = pad(parseInt(colour, 16).toString(2), 16, "0")
 
     // now use the binary to get colour values
     colour = {
-         r: parseInt(colour.substr(1, 5), 2),
-         g: parseInt(colour.substr(6, 5), 2),
+        r: parseInt(colour.substr(1, 5), 2),
+        g: parseInt(colour.substr(6, 5), 2),
         b: parseInt(colour.substr(11, 5), 2)
     }
 
     console.log(colour.r * 8 + ", " + colour.g * 8 + ", " + colour.b * 8)
 }
 
+// This should be Sonic's palette but god knows
 toPNG(['E0', 'FF', 'FF', 'FF', 'D6', 'DA', 'CE', '39', '84', '10', '60', 'FE', '00', '75', '80', '54', '00', '34', 'FF', 'CF', 'BF', 'AA', '53', '09', '9F', '10', '14', '00', '7F', '83', '60', '82'], true)
